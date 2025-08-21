0972c0aaeb9342f6bab198ee14cfeed7330fcc83
aman1181 <amans@zenithive.com>
2025-08-21 19:24:38 +0530

    med_stock2

--- server/server.js
+++ server/server.js
@@ -30,7 +30,7 @@ const app = express();
 
 // Middleware
 app.use(cors({
-  origin: ['http://localhost:3000', 'http://localhost:5173', 'http://localhost:5174'],
+  origin: process.env.FRONTEND_URL,
   credentials: true
 }));
 app.use(bodyParser.json());
